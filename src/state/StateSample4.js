640e1ac97b0126f023c154ebb38d3cc8d38bdba9
Tuğba Koç <ktugbaa7@gmail.com>
2023-08-08 18:23:40 +0300

    State sample

---
+++ src/state/StateSample4.js
@@ -0,0 +1,58 @@
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar';
+
+const StateSample4 = () => {
+
+  const [name, setname] = useState("");
+  const [surname, setsurname] = useState('');
+
+  const [users, setusers] = useState([]);
+
+  const add = () => {
+
+    let newUser = {
+      name,
+      surname
+    }
+
+    setusers([...users, newUser])
+    setname('')
+    setsurname('')
+
+  }
+
+
+  return (
+    <View>
+      <View>
+        <TextInput style={styles.input} value={name} onChangeText={setname} />
+      </View>
+      <View>
+        <TextInput style={styles.input} value={surname} onChangeText={setsurname} />
+      </View>
+      <View>
+        <Button title='add' onPress={add}></Button>
+      </View>
+      <View>
+        <FlatList
+          data={users}
+          renderItem={({item}) => <Text>{item.name} {item.surname}</Text>}
+        />
+      </View>
+      
+    </View>
+  )
+}
+
+export default StateSample4
+
+
+const styles = StyleSheet.create({
+  input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+  },
+});
