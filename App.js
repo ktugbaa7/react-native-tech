2612840eb92da50d019319e3f2f52d37463fd8a2
Tuğba Koç <ktugbaa7@gmail.com>
2023-08-17 17:52:53 +0300

    react navigation

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
-import SupplierPage from "./src/axiosSamples/SupplierPage";
-import LottieSample from "./src/lottieSample/LottieSample";
-import Customers from './src/reactquerysample/Customers';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ProductStack from './src/tabSample/stack/ProductStack';
+import ProfileStack from './src/tabSample/stack/ProfileStack';
+// import { QueryClient, QueryClientProvider } from 'react-query'
+// import SupplierPage from "./src/axiosSamples/SupplierPage";
+// import LottieSample from "./src/lottieSample/LottieSample";
+// import Customers from './src/reactquerysample/Customers';
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
 
-return <QueryClientProvider client={queryClient}>
+return <>
+<NavigationContainer>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Product" component={ProductStack} />
+        <Tab.Screen name="Profile" component={ProfileStack} />
+    </Tab.Navigator>
+</NavigationContainer>
+</>
+{/* <QueryClientProvider client={queryClient}>
 <Customers/>
-</QueryClientProvider>
+</QueryClientProvider> */}
 
 }
