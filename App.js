bd20486c0f362b1ba9ca4762868331a6dc1fc1f8
Tuğba Koç <ktugbaa7@gmail.com>
2023-08-22 17:01:38 +0300

    useContext sample

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import ProductStack from './src/tabSample/stack/ProductStack';
 import ProfileStack from './src/tabSample/stack/ProfileStack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CounterScreen from './src/context/CounterScreen';
+import { CounterProvider } from './src/context/CounterContext';
 // import { QueryClient, QueryClientProvider } from 'react-query'
 // import SupplierPage from "./src/axiosSamples/SupplierPage";
 // import LottieSample from "./src/lottieSample/LottieSample";
@@ -15,12 +18,15 @@ export default function App() {
 
 
 return <>
-<NavigationContainer>
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Product" component={ProductStack} />
-        <Tab.Screen name="Profile" component={ProfileStack} />
-    </Tab.Navigator>
-</NavigationContainer>
+<CounterProvider>
+            <NavigationContainer>
+                <Tab.Navigator screenOptions={{ headerShown: false }}>
+                    <Tab.Screen name="Product" component={ProductStack} />
+                    <Tab.Screen name="Profile" component={ProfileStack} />
+                    <Tab.Screen name="Counter" component={CounterScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </CounterProvider>
 </>
 {/* <QueryClientProvider client={queryClient}>
 <Customers/>
